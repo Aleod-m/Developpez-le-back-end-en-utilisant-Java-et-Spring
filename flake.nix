43ec49931551ff081849d51eebd4edf6ad2032c7
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2025-01-31 14:03:23 +0100

    Update devenv for using db.

--- flake.nix
+++ flake.nix
@@ -1,25 +1,34 @@
 {
   inputs = {
-    env-nix-pkgs-proxy.url = "github:Aleod-m/env-nix-pkgs-proxy";
+      nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+      devenv.url = "github:cachix/devenv";
   };
 
-  outputs = {self, env-nix-pkgs-proxy}:
-  let
-    proxyLib = env-nix-pkgs-proxy.lib;
-  in proxyLib.mkFlake {
-    perSystem = {pkgs, system}: {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs_22
-          typescript-language-server
-          mockoon
-          postman
-          mysql84
-          jdk17
-          typescript-language-server
-          maven
-        ];
-      };
+  outputs = {self, flake-parts, nixpkgs, ...} @ inputs:
+  flake-parts.lib.mkFlake {inherit inputs;} {
+    imports = [ inputs.devenv.flakeModule ];
+    systems  = nixpkgs.lib.systems.flakeExposed;
+    perSystem = {config, self', inputs', pkgs, system, ...}: {
+     devenv.shells.default = {
+       packages = with pkgs; [
+         nodejs_22
+         typescript-language-server
+         mockoon
+         postman
+         mysql84
+         jdk17
+         typescript-language-server
+         maven
+       ];
+       scripts = {
+         db-conn.exec = "${pkgs.mysql84}/bin/mysql -u root";
+       };
+       services.mysql = {
+         enable = true;
+         package = pkgs.mysql84;
+         initialDatabases = [ { name = "estate-db"; schema = ./ressources/sql/script.sql; } ];
+       };
+     };
     };
   };
 }
